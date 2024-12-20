a45bb4be9b611b048572d8cf7aaa7597c3a8fae9
LightVl <mihail.rataev@gmail.com>
2024-12-20 04:46:13 +0300

    gateway

--- src/main/java/ru/neoflex/education/PrepareToBattle.java
+++ src/main/java/ru/neoflex/education/PrepareToBattle.java
@@ -9,10 +9,22 @@ public class PrepareToBattle implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+        int warriors = (int) delegateExecution.getVariable("warriors");
         int enemyWarriors = (int) (Math.random() * 100);
         String battleStatus = "Undefined";
+        boolean isWin = false;
+
+
+        if ((warriors - enemyWarriors) > 0) {
+            isWin = true;
+            battleStatus = "Victory!";
+        } else {
+            battleStatus = "Fail :(";
+        }
+
 
         delegateExecution.setVariable("enemyWarriors", enemyWarriors);
         delegateExecution.setVariable("battleStatus", battleStatus);
+        delegateExecution.setVariable("isWin", isWin);
     }
 }
